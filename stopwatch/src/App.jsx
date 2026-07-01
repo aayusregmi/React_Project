93cc7eb5e0495df7bcbbd4fa9931da9765150bf5
Aayus Regmi <aayusregmi721@gmail.com>
2026-07-01 21:46:26 +0545

    Learn about the custom hook , useid and optimization

--- stopwatch/src/App.jsx
+++ stopwatch/src/App.jsx
@@ -1,5 +1,5 @@
-import { useCallback, useEffect, useState } from 'react'
-import './App.css'
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -7,13 +7,14 @@ function App() {
   const [isStopClick, setStop] = useState(false);
   const [isResetClick, setReset] = useState(false);
   const countFn = useCallback(() => {
+    console.log("We are at countFn");
+
     if (isResetClick) {
       setCount(0);
     } else if (isStartClick) {
       setCount((prev) => prev + 1);
     }
-
-  }, [isStartClick, isResetClick, isResetClick]);
+  }, [isStartClick , isResetClick]);
 
   function start() {
     setStart(true);
@@ -27,30 +28,40 @@ function App() {
   }
 
   useEffect(() => {
-      const id = setInterval(() => {
+    let id;
+     if(isStartClick){
+      console.log("For 1 sec.....");
+       id = setInterval(() => {
         countFn();
-      }, 1000)
-
-
-
+      }, 1000);
+     }
+      
+  
     return () => clearInterval(id);
-
   }, [isStartClick, isStopClick, isResetClick]);
 
+  console.log("I am below the useEffect!!!");
+  
   return (
     <>
-      <div className='bg-black h-screen w-full flex justify-center items-center flex-col'>
-        <div className='Hero w-1/2 h-20 outline-none border-white bg-white rounded-xl'>
-          <h1 className='text-4xl text-black  text-center my-3'>{count}</h1>
+      <div className="bg-black h-screen w-full flex justify-center items-center flex-col">
+        <div className="Hero w-1/2 h-20 outline-none border-white bg-white rounded-xl">
+          <h1 className="text-4xl text-black  text-center my-3">{count}</h1>
         </div>
-        <div className='flex flex-wrap bg-gray-500 w-1/2 h-11 text-white rounded-xl my-2 flex-row justify-around items-center'>
-          <button className='bg-green-600 rounded-md' onClick={start}>Start</button>
-          <button className='bg-green-600 rounded-md' onClick={stop}>Stop</button>
-          <button className='bg-green-600 rounded-md' onClick={reset}>Reset</button>
+        <div className="flex flex-wrap bg-gray-500 w-1/2 h-11 text-white rounded-xl my-2 flex-row justify-around items-center">
+          <button className="bg-green-600 rounded-md" onClick={start}>
+            Start
+          </button>
+          <button className="bg-green-600 rounded-md" onClick={stop}>
+            Stop
+          </button>
+          <button className="bg-green-600 rounded-md" onClick={reset}>
+            Reset
+          </button>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
