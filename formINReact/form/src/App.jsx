0f35e2c4c7f2017add53e9cb5be1a0709c025d34
Aayus Regmi <aayusregmi721@gmail.com>
2026-07-21 14:41:35 +0545

    Learned to handle multiple inputs

--- formINReact/form/src/App.jsx
+++ formINReact/form/src/App.jsx
@@ -2,19 +2,31 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [name, setName] = useState("");
+  const[info , setInfo]  = useState({
+    name : "",
+    userName : "",
+  })
   console.log("re-render");
-
+     
+  const handleInputChange = (e) =>{
+  const field = e.target.name
+  const value = e.target.value
+  setInfo((prev) =>{
+    prev[field] = value
+    return{
+      ...prev
+    }
+  })
+  }
+  
   return (
-    <>
+    <div className='bg-gray-600 flex flex-col h-50 w-60'>
      <form>
-      <input type="text" value={name} placeholder='Enter you name' onChange={(e)=>{
-        setName(e.target.value)
-      }}/>
+      <input className='bg-white ' value={info.name} name='name' type="text"  placeholder='Enter your name' onChange={handleInputChange}/>
+      <input className='bg-white' value={info.userName} name='userName' type="text"  placeholder='Enter your Username' onChange={handleInputChange}/>
       <button>Submit</button>
-
      </form>
-    </>
+    </div>
   )
 }
 
--- toDoList/src/Component/Todolist/Todolist.jsx
+++ toDoList/src/Component/Todolist/Todolist.jsx
@@ -1,76 +1,104 @@
 import React, { useState } from "react";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const Todolist = () => {
-  const [todos , setTodos] = useState([])
-  const [newTodo , setNewtodo] = useState([""]);
-  const [text , setText] = useState("UpperCase")
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewtodo] = useState([""]);
+  const [isDone, setIsDone] = useState(false);
 
-  const handleEnter = (event) =>{
-      event.preventDefault();
-      setTodos([...todos , {task:newTodo , key:uuidv4()}])
-      setNewtodo("");
-  }
-  
-  const handleNewtodo = (e) =>{
+  const handleEnter = (event) => {
+    event.preventDefault();
+    setTodos([...todos, { task: newTodo, key: uuidv4() }]);
+    setNewtodo("");
+  };
+
+  const handleNewtodo = (e) => {
     setNewtodo(e.target.value);
-  }
+  };
 
-  const handleDlt = (id) =>{
+  const handleDlt = (id) => {
     console.log(id);
-    setTodos(todos.filter((todo) => todo.key != id ))
-  }
+    setTodos(todos.filter((todo) => todo.key != id));
+  };
 
-  const handleUppercase = () =>{
-    setTodos(todos.map((todo)=>{
-      return{
-         ...todo ,
-         task: todo.task.toUpperCase()
-      }
-    })) 
-  }
-   
-  const handleCaseChange = (id) =>{
-     setTodos(todos.map((todo)=>{
-        if(todo.key === id){
-          return{
-            ...todo , 
-            task: todo.task.toUpperCase()
-          }
-        }else{
-          return{
-            ...todo
-          }
+  const handleUppercase = () => {
+    setTodos(
+      todos.map((todo) => {
+        return {
+          ...todo,
+          task: todo.task.toUpperCase(),
+        };
+      }),
+    );
+  };
+
+  const handleMarkAsDone = (id) => {
+    todos.map((todo) => {
+      if (todo.key == id) {
+        if (isDone == false) {
+          setIsDone(true);
+        } else {
+          setIsDone(false);
         }
-      })) 
-    if(text === "UpperCase"){
-      setText("LowerCase")
-    }else{
-      setText("Uppercase")
-    }
-  }
-  
-
+      }
+    })
+  };
   return (
     <div className="w-screen h-screen flex justify-center items-center text-white">
       <div className="bg-gray-600 w-1/2 h-40">
         <div className="inputSection px-4">
-          <form><input value={newTodo} type="text" placeholder="Enter Your Task" className="inputText outline-0" onChange={handleNewtodo}/>
-          <button type="submit" className="bg-green-500 rounded-xl w-20 ml-3 p-1" onClick={handleEnter}>Enter</button>
+          <form>
+            <input
+              value={newTodo}
+              type="text"
+              placeholder="Enter Your Task"
+              className="inputText outline-0"
+              onChange={handleNewtodo}
+            />
+            <button
+              type="submit"
+              className="bg-green-500 rounded-xl w-20 ml-3 p-1"
+              onClick={handleEnter}
+            >
+              Enter
+            </button>
           </form>
         </div>
-        <div className="px-7 flex flex-col"> {
-        todos.map((todo)=>
-          <li key={todo.key}>{todo.task || todo}<button value={todo.key} className="btnDlt ml-4 bg-blue-400 rounded-xl text-sm px-1" onClick={() => handleDlt(todo.key)}>Delete</button>
-          <button value={todo.key} className="btnDlt ml-4 bg-blue-400 rounded-xl text-sm px-1" onClick={() => handleCaseChange(todo.key)}>{text}</button>
-          </li>
-        ) 
-      }
+        <div className="px-7 flex flex-col">
+          {" "}
+          {todos.map((todo) => (
+            <li
+              style={{
+                textDecoration: isDone ? "line-through" : "none",
+              }}
+              key={todo.key}
+            >
+              {todo.task || todo}
+              <button
+                value={todo.key}
+                className={`btnDlt ml-4 bg-blue-400 rounded-xl text-sm px-1`}
+                onClick={() => handleDlt(todo.key)}
+              >
+                Delete
+              </button>
+              <button
+                value={todo.key}
+                className="btnDlt ml-4 bg-blue-400 rounded-xl text-sm px-1"
+                onClick={() => handleMarkAsDone(todo.key)}
+              >
+                Mark As Done
+              </button>
+            </li>
+          ))}
+        </div>
       </div>
-      </div>
-      <button className="bg-orange-500 rounded-2xl px-4 ml-4" onClick={handleUppercase}>UpperCase Todo's</button>
+      <button
+        className="bg-orange-500 rounded-2xl px-4 ml-4"
+        onClick={handleUppercase}
+      >
+        UpperCase Todo's
+      </button>
     </div>
-
   );
 };
 
